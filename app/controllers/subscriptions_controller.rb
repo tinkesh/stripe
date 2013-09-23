3381ef4dd070e71a2a6db7765d3aced8ebd3542e
tinkesh <happywithrails@gmail.com>
2013-09-23 07:19:57 +0530

    applied modifications

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,7 +5,21 @@ class SubscriptionsController < ApplicationController
   end
 
 def create
-  customer = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: params[:subscription].to_i)
+
+  #currency = params[:currency]
+  if !params[:yearly_subscription].blank?
+    subscription = params[:yearly_subscription].to_i
+  elsif !params[:monthly_subscription].blank?
+    subscription = params[:monthly_subscription].to_i
+  else
+    subscription = 1
+  end
+
+  if !params[:coupon].blank?
+    customer = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription, coupon: params[:coupon])
+  else
+    customer = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription)
+  end
  @response = customer
  render :response 
 end
