1ebc0a7e61f8e239fe9a32a782fec2235ba8577d
tinkesh <happywithrails@gmail.com>
2013-09-25 13:48:27 +0530

    added one-off payment and also added pay through canada currency

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,21 +6,42 @@ class SubscriptionsController < ApplicationController
 
 def create
 
-  #currency = params[:currency]
-  if !params[:yearly_subscription].blank?
-    subscription = params[:yearly_subscription].to_i
-  elsif !params[:monthly_subscription].blank?
-    subscription = params[:monthly_subscription].to_i
-  else
-    subscription = 1
-  end
+ if params[:recurring] == "1"
 
-  if !params[:coupon].blank?
-    customer = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription, coupon: params[:coupon])
-  else
-    customer = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription)
-  end
- @response = customer
+	  if !params[:yearly_subscription].blank?
+	    subscription = params[:yearly_subscription].to_i
+	  elsif !params[:monthly_subscription].blank?
+	    subscription = params[:monthly_subscription].to_i
+	  else
+	    subscription = 1
+	  end
+          begin
+	  if !params[:coupon].blank?
+	    @response = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription, coupon: params[:coupon])
+	  else
+	    @response = Stripe::Customer.create(description: "Customer for #{params[:stripeToken]}", card: params[:stripeToken], plan: subscription)
+	  end
+
+	  rescue Stripe::InvalidRequestError => e
+	  raise e
+	  end
+
+
+ else
+
+	# Create the charge on Stripe's servers - this will charge the user's card
+	begin
+	  @response = Stripe::Charge.create(
+	    :amount => params[:amount].to_i,
+	    :currency => params[:currency],
+	    :card => params[:stripeToken],
+	    :description => params[:payoff_description]
+	  )
+	rescue Stripe::CardError => e
+	  raise e
+	end
+ end
+
  render :response 
 end
 
